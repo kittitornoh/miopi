a351920489b7f902d19917710b9ca397996d63de
kittitornoh <kithodges95@gmail.com>
2021-06-08 08:49:15 -0400

    added env variables to sequelize config

--- server/index.js
+++ server/index.js
@@ -12,10 +12,13 @@ app.use(cors());
 app.use(express.json());
 
 // database
+const db = require('./models');
 
 // routers
 
 // init server
-app.listen(PORT, () => {
-  console.log(`Server is running on: http://localhost:${PORT}`);
+db.sequelize.sync().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on: http://localhost:${PORT}`);
+  });
 });
