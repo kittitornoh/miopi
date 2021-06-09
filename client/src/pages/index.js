cf68de83e656bd252eb07f312d2c56e563449861
kittitornoh <kithodges95@gmail.com>
2021-06-09 13:23:19 -0400

    setup folder structure

---
+++ client/src/pages/index.js
@@ -0,0 +1,7 @@
+import { Home } from './Home/home';
+import { Login } from './Login/login';
+import { Post } from './Post/post';
+import { Profile } from './Profile/profile';
+import { Register } from './Register/register';
+
+export { Home, Login, Post, Profile, Register };
