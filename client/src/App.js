926330c89edce570ea15dde51cbb4ab8785753f8
kittitornoh <kithodges95@gmail.com>
2021-06-09 13:06:49 -0400

    installed TailwindCSS

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,5 @@
-import './App.css';
-
 const App = () => {
-  return <div className='App'></div>;
+  return <div>Hello world.</div>;
 };
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+// components
 import App from './App';
+
+// styles
+import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
